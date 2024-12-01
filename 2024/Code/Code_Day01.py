b33f9f0eb6110ba865d71b10449843314df6258c
Scott Sutton <sssutton10@gmail.com>
2024-12-01 09:35:12 -0500

    Day 1 Solution

--- 2024/Code/Code_Day01.py
+++ 2024/Code/Code_Day01.py
@@ -1,1 +1,20 @@
-sdaf
+with open('C:/Users/sssut/Documents/Python Scripts/Advent of Code/2024/Inputs/Input_Day01.txt', 'r') as f:
+    nums = f.readlines()
+
+l1 = [None] * len(nums)
+l2 = [None] * len(nums)
+
+for i, n in enumerate(nums):
+    n1, n2 = n.split('   ')
+    l1[i] = int(n1)
+    l2[i] = int(n2)
+    
+l1 = sorted(l1)
+l2 = sorted(l2)
+
+print(sum([abs(x - y) for x, y in zip(l1, l2)]))
+
+p2 = 0
+for x in l1:
+    p2 += x * l2.count(x)
+print(p2)
